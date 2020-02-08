53507bf707ebf12aeb997300d8279fcf92c5dd4c
ergou <15066090444@qq.com>
2020-02-08 20:24:03 +0800

    可以选中或取消tag

--- src/components/money/Tags.vue
+++ src/components/money/Tags.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="tags">
         <ul class="current">
-            <li>衣</li>
-            <li>食</li>
-            <li>住</li>
-            <li>行</li>
+            <!--这个地方设置class的方式真的很棒啊-->
+            <li v-for="tag in tagsContent" @click="toggle(tag)" :class="{selected:selectedTag.indexOf(tag)>=0}">
+                {{tag}}
+            </li>
         </ul>
         <div class="new">
             <button>新增标签</button>
@@ -13,25 +13,43 @@
 </template>
 
 <script lang="ts">
-    export default {
-        name: "Tags"
+    import Vue from "vue";
+    import {Component, Prop} from "vue-property-decorator";
+
+    @Component
+    export default class Tags extends Vue {
+        name: "Tags";
+        @Prop() tagsContent: string[] | undefined;
+        selectedTag: string[] = [];
+
+        toggle(tag: string) {
+            const index = this.selectedTag.indexOf(tag);
+            if (index >= 0) {
+                this.selectedTag.splice(index, 1);
+            } else {
+                this.selectedTag.push(tag);
+            }
+        }
+
+
     };
 </script>
 
 <style scoped lang="scss">
     @import "~@/assets/style/helper.scss";
+
     .tags {
         font-size: 14px;
         background-color: #fff;
         padding-bottom: 10px;
 
-        .current{
+        .current {
             display: flex;
             min-height: 180px;
             align-items: flex-start;
             padding: 4px 0;
 
-            li{
+            li {
                 border: 1px solid #ccc;
                 display: inline-block;
                 /*这里我没有直接设置宽度，而是用padding进行撑开*/
@@ -42,9 +60,14 @@
                 border-radius: 4px;
                 margin-left: 4px;
             }
+
+            li.selected {
+                background-color: $color-main-tonal;
+            }
         }
-        .new{
-            button{
+
+        .new {
+            button {
                 background-color: $color-main-tonal;
                 padding: 0 10px;
                 height: 24px;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,8 +3,8 @@
         <!--Layout是一个插槽-->
         <Layout>
             <!--因为，你这里要传入的是一个number，所以这个我在propA前面加了引号-->
-            <Header :propA="222"/>
-            <Tags/>
+            <Header/>
+            <Tags :tags-content="tags"/>
             <Calcul/>
 
 
@@ -14,12 +14,21 @@
 </template>
 
 <script lang="ts">
-    import Tags from '@/components/money/Tags.vue';
-    import Header from '@/components/money/Header.vue';
-    import Calcul from '@/components/money/Calcul.vue';
-    export default {
-        name: "Money",
-        components: {Calcul, Header, Tags}
+    import Vue from "vue";
+    import Tags from "@/components/money/Tags.vue";
+    import Header from "@/components/money/Header.vue";
+    import Calcul from "@/components/money/Calcul.vue";
+    import {Component, Prop} from "vue-property-decorator";
+
+
+    @Component({
+        components:{
+            Tags,Header,Calcul
+        }
+    })
+    export default class Money extends Vue {
+        name: "Money";
+        tags:string[] = ['衣','食']
     };
 </script>
 
